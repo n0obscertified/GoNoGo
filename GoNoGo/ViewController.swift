0844b3db1bace1c76e3c20c5a50e0b9ff2ab0295
Jeel <jpatel24@luc.edu>
2016-07-26 17:17:45 -0500

    adding Facebook login and logout

--- GoNoGo/ViewController.swift
+++ GoNoGo/ViewController.swift
@@ -10,73 +10,81 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController, FBSDKLoginButtonDelegate {
-
+    
     @IBOutlet weak var FbLogIn: FBSDKLoginButton!
     
     let imagePicker: UIImagePickerController! = UIImagePickerController()
     
     override func viewDidLoad() {
+        self.FbLogIn.delegate = self;
         super.viewDidLoad()
-        self.FbLogIn.delegate = self
+        
         
         if (FBSDKAccessToken.currentAccessToken() != nil)
         {
             print("User is already loggen in....")
-            // User is already logged in, do work such as go to next view controller.
+            let cameraViewController = self.storyboard!.instantiateViewControllerWithIdentifier("TabControl")
+            print("navigating to the next view controller from view did load")
+            
+            self.navigationController!.pushViewController(cameraViewController, animated: true)
+            
         }
         else
         {
             self.FbLogIn.readPermissions = ["public_profile", "email"]
-         //   FbLogIn = FBSDKLoginButton()
-          //  self.view.addSubview(FbLogIn)
-           // FbLogIn.center = self.view.center
-           // FbLogIn.readPermissions = ["public_profile", "email"]
-           // self.FbLogIn.delegate = self
             
         }
         
-        
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-
- 
-
     
     func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!)
     {
-     
-       
+        if ((error) != nil)
+        {
+            // Process error
+        }
+        else if result.isCancelled {
+            // Handle cancellations
+        }
+        else {
+            // If you ask for multiple permissions at once, you
+            // should check if specific permissions missing
+            if result.grantedPermissions.contains("email")
+            {
+                print(result)
+                let cameraViewController = storyboard!.instantiateViewControllerWithIdentifier("TabControl")
+                print("navigating to the next view controller")
+                
+                self.navigationController!.pushViewController(cameraViewController, animated: true)
+                
+            }
+        }
     }
-   
+    
     func loginButtonDidLogOut(loginButton: FBSDKLoginButton!)
     {
+        print("Print User logged out")
+        
+        FBSDKAccessToken.setCurrentAccessToken(nil)
+        FBSDKProfile.setCurrentProfile(nil)
+        
+        let deletepermission = FBSDKGraphRequest(graphPath: "me/permissions/", parameters: nil, HTTPMethod: "DELETE")
+        deletepermission.startWithCompletionHandler({(connection,result,error)-> Void in
+            print("the delete permission is \(result)")
+            
+        })
+        
+        let loginManager = FBSDKLoginManager()
+        loginManager.logOut()
         
     }
     
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-        print("Got an image")
-        if let pickedImage:UIImage = (info[UIImagePickerControllerOriginalImage]) as? UIImage {
-            let selectorToCall = Selector("imageWasSavedSuccessfully:didFinishSavingWithError:context:")
-            UIImageWriteToSavedPhotosAlbum(pickedImage, self, selectorToCall, nil)
-        }
-        imagePicker.dismissViewControllerAnimated(true, completion: {
-            // Anything you want to happen when the user saves an image
-        })
-    }
     
-    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
-        print("User canceled image")
-        dismissViewControllerAnimated(true, completion: {
-            // Anything you want to happen when the user selects cancel
-        })
-    }
-
 }
 
