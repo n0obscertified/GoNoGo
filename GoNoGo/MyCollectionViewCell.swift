d2bf8f596d11aa40ef1e1dff52551cb775aff68b
Jeel <jpatel24@luc.edu>
2016-07-28 19:19:14 -0500

    started working on profile view and change transition style

---
+++ GoNoGo/MyCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MyCollectionViewCell.swift
+//  GoNoGo
+//
+//  Created by Jeel on 7/28/16.
+//  Copyright © 2016 Ismael. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var myLabel: UILabel!
+}
