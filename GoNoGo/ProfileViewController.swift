3e668f37113934a9047c06dfbedbe250cd1eedf5
Jeel <jpatel24@luc.edu>
2016-07-29 03:02:02 -0500

    added fetching images from database

--- GoNoGo/ProfileViewController.swift
+++ GoNoGo/ProfileViewController.swift
@@ -8,24 +8,64 @@
 
 import UIKit
 import FirebaseStorage
+import Firebase
 import FirebaseAuth
+import SwiftCompressor
 class ProfileViewController: UIViewController, UICollectionViewDataSource {
 
-    var myArray = [""]
+    var myArray = [String]()
     let reuseIdentifier = "cell"
     
-    let storage = FIRStorage.storage()
+    let database = FIRDatabase.database().reference()
     
-    override func viewDidLoad() {
+    @IBOutlet weak var image: UIImageView!
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        FIRAuth.auth()?.addAuthStateDidChangeListener({ (auth, user) in
-            if let user = user{
-              let base = self.storage.referenceForURL("gs://gonogo-5d73d.appspot.com/\(user.uid)")
-                print(base.description)
-            }
-            
+        FIRAuth.auth()?.addAuthStateDidChangeListener(
+            {
+                (auth, user) in
+                if let currentUsr  = user
+                {
+                    dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INTERACTIVE, 0), {
+                        self.database.child("Users").observeEventType(.ChildAdded, withBlock: { snapshot in
+                            let userId = snapshot.key
+                            for (x, imageKey) in  snapshot.children.enumerate()
+                            {
+                                if imageKey.hasChildren(){
+                                    var lines = ""
+                                    var index = 0
+                                    for i in imageKey.children{
+                                        lines.appendContentsOf(i.value!!)
+
+                                    }
+                                    
+                                    
+                                    do  { let imageData = try NSData(base64EncodedString: lines ,
+                                        options: NSDataBase64DecodingOptions.IgnoreUnknownCharacters)?.decompress()
+                                        
+                                        let decodedImage = UIImage(data:imageData!)
+                                        dispatch_async(dispatch_get_main_queue(), {
+                                            self.image.image = decodedImage
+                                        })
+                                        
+                                        break;
+                                        
+                                    }
+                                    catch{}
+                                    
+                                    
+                                }
+                                
+                            }
+                    })
+
+
+                        
+                    })
+                }
         })
-        // Do any additional setup after loading the view.
+
     }
 
     override func didReceiveMemoryWarning() {
